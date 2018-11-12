edc5238210397934eb4d8671c8727ad5a16e5608
karamahfid <44231117+karamahfid@users.noreply.github.com>
2018-11-12 14:59:49 +0400

    Update LA9.java

--- src/la9/LA9.java
+++ src/la9/LA9.java
@@ -14,8 +14,18 @@ public class LA9 {
     /**
      * @param args the command line arguments
      */
+       static public void featureA()
+    {
+        System.out.println("Feature B added");
+    }
+    static public void featureB()
+    {
+        System.out.println("Feature A added");
+    }
     public static void main(String[] args) {
         // TODO code application logic here
+        featureA();
+        featureB();
     }
     
 }
